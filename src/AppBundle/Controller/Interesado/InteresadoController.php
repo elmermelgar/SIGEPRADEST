3daae07b9f65b91e66a78b4a74a8b26c1d8e84c3
UserPC\User <oscar.desarrollador@gmail.com>
2016-09-23 23:41:47 -0600

    Registro de usuarios interesados

--- src/AppBundle/Controller/Interesado/InteresadoController.php
+++ src/AppBundle/Controller/Interesado/InteresadoController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Interesado;
 
+use AppBundle\Entity\Usuario;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,8 +13,42 @@ class InteresadoController extends Controller
     /**
      * @Route("/interesado/register", name="register")
      */
-    public function registerAction(){
-        return $this->render('AppBundle:Interesado:registro.html.twig');
+    public function registerAction(Request $request){
+        if ($request->isMethod('POST'))
+        {
+            $em=$this->getDoctrine()->getManager("default");
+            //Creando usuario
+            $u = new Usuario();
+            $u->setNomusuario($request->get("usuario"));
+            $u->setNombre($request->get("nombre"));
+            //interesado = 4
+            //$u->setIdRol($em->getRepository('AppBundle:Roles')->find('4'));
+            $u->setIdRol('4');
+            $u->setCorreo($request->get("email"));
+            $u->setApellido($request->get("apellido"));
+            $u->setIsactive(1);
+            //Cifra la contrase?a
+            $factory = $this->get('security.encoder_factory');
+            $encoder = $factory->getEncoder($u);
+            $password = $encoder->encodePassword($request->get("password"), $u->getSalt());
+            $u->setPassword($password);
+            //Persistencia
+            $em->persist($u);
+            $em->flush();
+            //redireccionamiento
+            return $this->redirectToRoute('interesado');
+        }else {
+            $interesado = new Usuario();
+            return $this->render('AppBundle:Interesado:registro.html.twig', array('interesado' => $interesado));
+        }
+    }
+
+    /**
+     * @Route("/interesado/register/create", name="register_create")
+     */
+    public function registerCreateAction(){
+        $interesado = new Usuario();
+        return $this->render('AppBundle:Interesado:registro.html.twig',array('interesado' => $interesado));
     }
 
 
