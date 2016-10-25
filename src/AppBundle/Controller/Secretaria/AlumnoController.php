04c812cd29067eaa0602a20382f4fa51836c36ff
fhmen <fh.menendez@outlook.com>
2016-10-25 00:20:39 -0600

    Creacion de nuevo alumno a partir de una aspirante, crea registro curriculum y cambia de rol al usuario. Botones de curruculum y datos personales funcionales

---
+++ src/AppBundle/Controller/Secretaria/AlumnoController.php
@@ -0,0 +1,304 @@
+<?php
+
+namespace AppBundle\Controller\Secretaria;
+use Proxies\__CG__\AppBundle\Entity\Curriculum;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Entity\Alumno;
+use AppBundle\Form\AlumnoType;
+
+/**
+ * Alumno controller.
+ *
+ * @Route("/secretaria/alumnos")
+ */
+class AlumnoController extends Controller
+{
+
+    /**
+     * Lists all Alumno entities.
+     *
+     * @Route("/", name="alumnos")
+     * @Method("GET")
+     * @Template("@App/Secretaria/Alumno/newSolAproba.html.twig")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        //usamos esta consulta para ver las solicitudes que no contienen usuarios. por lo que mostraremos solo solicitudes que tengan aspirantes
+//        $entities = $em->createQuery('Select soli from AppBundle:Solicitud soli ')->getResult();
+        $entities = $em->createQuery('Select soli,use from AppBundle:Solicitud soli JOIN soli.idUi use WHERE use.idRol = 4  ')->getResult();
+
+//        $entities = $em->getRepository('AppBundle:Solicitud')->findBy( array('name' => 'someValue'));
+//  var_dump($entities[0]->getIdAlumno());
+//      echo $entities[0];
+        return array(
+            'entities' => $entities,
+        );
+    }
+    /**
+     * Creates a new Alumno entity.
+     *
+     * @Route("/", name="alumno_create")
+     * @Method("POST")
+     * @Template("AppBundle:Secretaria/Alumno:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+
+        $entity = new Alumno();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('alumno_show', array('id' => $entity->getIdAlumno())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a form to create a Alumno entity.
+     *
+     * @param Alumno $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Alumno $entity)
+    {
+        $form = $this->createForm(new AlumnoType(), $entity, array(
+            'action' => $this->generateUrl('alumno_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Alumno entity.
+     *
+     * @Route("/new", name="alumno_new")
+     * @Method("GET")
+     * @Template("@App/Secretaria/Alumno/new.html.twig")
+     */
+    public function newAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        //obtenemos las variables de los header
+        $idUsuario = $request->get("usuario");
+        $idDatosPersonales =$request->get("datosPer");
+
+        //obtenemos el numero de usuarios registrados el cual para ser insertado debe de ser cero
+        $entityUsuario2 = $em->getRepository('AppBundle:Alumno')->findByidUi( $idUsuario);
+        $numeroUsuarios = count( $entityUsuario2);
+
+        if($numeroUsuarios== 0){
+
+         // seteamoas las nueva entidades a guardar
+        $entityCurruculum= new Curriculum();
+        $entityAlumno = new Alumno();
+
+        // obtenemos las entidadedes
+        $entityUsuario = $em->getRepository('AppBundle:Usuario')->find( $idUsuario);
+        $entityDatosPer= $em->getRepository('AppBundle:DatosPersonales')->find( $idDatosPersonales);
+        $rolFromEntity =$em->getRepository('AppBundle:Roles')->find(3);
+        //obtenemos el numero de usuarios registrados el cual para ser insertado debe de ser cero
+
+
+//var_dump( count( $entityUsuario2));
+//var_dump($entityUsuario);
+//var_dump($entityDatosPer);
+
+        //seteamos los datos personales
+        $entityUsuario->setIdRol($rolFromEntity);
+
+        // seteamos el entity alumno
+        $entityAlumno->setEstadoAlumno("activo");
+        $entityAlumno->setIdUi($entityUsuario);
+        $entityAlumno->setIdDp($entityDatosPer);
+
+           $entityCurruculum->setIdAlumno($entityAlumno);
+
+
+        $em->persist($entityAlumno);
+        $em->persist($entityCurruculum);
+
+
+
+            $em->flush();
+
+
+            var_dump($entityAlumno->getIdDp());
+        }
+
+
+
+//
+        return array(
+            'exito' => "Usuario guardado correctamente",
+            'general'   => "Saludos",
+            'idDatosPerson'   => $entityAlumno->getIdDp()->getIdDp(),
+            'idAlum'   => $entityAlumno->getIdAlumno(),
+//            'id'   => 1,
+        );
+    }
+
+    /**
+     * Finds and displays a Alumno entity.
+     *
+     * @Route("/{id}", name="alumno_show")
+     * @Method("GET")
+     * @Template("@App/Secretaria/Alumno/show.html.twig")
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Alumno')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Alumno entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Alumno entity.
+     *
+     * @Route("/{id}/edit", name="alumno_edit")
+     * @Method("GET")
+     * @Template("@App/Secretaria/Alumno/edit.html.twig")
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Alumno')->findOneByidDp($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Alumno entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+    * Creates a form to edit a Alumno entity.
+    *
+    * @param Alumno $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Alumno $entity)
+    {
+        $form = $this->createForm(new AlumnoType(), $entity, array(
+            'action' => $this->generateUrl('alumno_update', array('id' => $entity->getIdAlumno())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Alumno entity.
+     *
+     * @Route("/{id}", name="alumno_update")
+     * @Method("PUT")
+     * @Template("AppBundle:Alumno:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Alumno')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Alumno entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('alumno_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    /**
+     * Deletes a Alumno entity.
+     *
+     * @Route("/{id}", name="alumno_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AppBundle:Alumno')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Alumno entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('alumno'));
+    }
+
+    /**
+     * Creates a form to delete a Alumno entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('alumno_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Form/CurriculumType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CurriculumType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('idAlumno','entity',array('class'=>'AppBundle\Entity\Alumno','property' =>'idAlumno'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Curriculum'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_curriculum';
+    }
+}
