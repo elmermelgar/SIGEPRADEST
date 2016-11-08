0d8d710229d5f02824120c3c93288c18c0048307
elmer_melgar <elmermelgar999@gmail.com>
2016-11-08 00:29:38 -0600

    Modificaciones

---
+++ src/AppBundle/Controller/Doctores/ModulosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Controller\Doctores;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Controller\SecurityController;
+
+class ModulosController extends SecurityController
+{
+
+
+}
