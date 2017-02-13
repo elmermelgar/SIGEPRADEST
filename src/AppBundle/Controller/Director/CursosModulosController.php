c0a2d294ffe43347888a211de23e517015422025
elmer_melgar <elmermelgar999@gmail.com>
2017-02-12 18:27:18 -0600

    Detalles

--- src/AppBundle/Controller/Director/CursosModulosController.php
+++ src/AppBundle/Controller/Director/CursosModulosController.php
@@ -22,4 +22,46 @@ class CursosModulosController extends DSIController
         $cursos=$em->getRepository('AppBundle:Curso')->findAll();
         return $this->render('AppBundle:Director/Curso:historialcursosdirector.html.twig', array('cursos'=>$cursos));
     }
+    /**
+     * @Route("/director/curso_ver/{id}",name="detallesCursoDirector")
+     */
+    public function detallesCursoAction($id)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $doc = $em->getRepository("AppBundle:Doctores")->findBy(array(),array('nombreDoc'=>'ASC'));
+        $tc=$em->getRepository("AppBundle:TipoCurso")->findAll();
+        $datos=$em->getRepository('AppBundle:Curso')->find($id);
+        $hc=$em->getRepository('AppBundle:HorarioCurso')->findOneBy(array('idCurso'=> $id));
+        $id_hc=$hc->getIdHc();
+        $hc=$em->getRepository('AppBundle:HorarioCurso')->find($id_hc);
+        $d1=$this->mostrarD1s($id);
+        $cuota=$em->getRepository('AppBundle:Cuotas')->findBy(array('idCurso'=> $id));
+
+        return $this->render("AppBundle:Director/Curso:curso_ver.html.twig",array("tc"=>$tc,'datos'=>$datos,"doc"=>$doc,"hc"=>$hc,"d1"=>$d1, 'cuota'=>$cuota));
+    }
+    /**
+     * @Route("/director/cursos",name="verCursoDirector")
+     */
+    public function verCursoDirectorAction()
+    {
+        if($this->getUser()){
+            $em=$this->getDoctrine()->getManager("default");
+            $cursos=$em->getRepository('AppBundle:Curso')->findAll();
+            $hc=$em->createQuery('select curso from AppBundle:HorarioCurso curso ORDER BY curso.fechaInicio DESC ')->getResult();
+
+            $fechaAhora=(new \DateTime('now',new \DateTimeZone('America/El_Salvador')))->format("Y-m-d");
+            for($i=0;$i<count($hc);$i++){
+                if($fechaAhora>($hc[$i]->getFechaFin()->format("Y-m-d"))){
+                    $id=$em->getRepository('AppBundle:Curso')->find($hc[$i]->getIdCurso());
+                    $id->setEstadoCurso("finanlizado");
+                    //Guardar en la BD
+                    $em->flush();
+                }
+            }
+            return $this->render('AppBundle:Director/Curso:curso.html.twig', array('cursos'=>$cursos,'hc'=>$hc));
+        }else{
+            return $this->redirectToRoute('login');
+        }
+
+    }
 }
